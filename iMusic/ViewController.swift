29098c8023ab890607fd6986f2f48c0d5a6897e5
a.pride <a.pride.msk@gmail.com>
2020-08-17 13:37:46 +0300

    Initial Commit

---
+++ iMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iMusic
+//
+//  Created by Александр Прайд on 17.08.2020.
+//  Copyright © 2020 Alexander Pride. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
