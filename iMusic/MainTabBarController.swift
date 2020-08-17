5b68e655081c5d044188b37337a08fc23fdd6fbe
a.pride <a.pride.msk@gmail.com>
2020-08-17 14:50:25 +0300

    MainTabBarControllerMainTabBarController

---
+++ iMusic/MainTabBarController.swift
@@ -0,0 +1,47 @@
+//
+//  MainTabBarController.swift
+//  iMusic
+//
+//  Created by Александр Прайд on 17.08.2020.
+//  Copyright © 2020 Alexander Pride. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        tabBar.tintColor = #colorLiteral(red: 1, green: 0, blue: 0.3764705882, alpha: 1)
+        
+        viewControllers = [
+            generateViewController(rootViewController: SearchViewController(), image: #imageLiteral(resourceName: "search"), title: "Search"),
+            generateViewController(rootViewController: ViewController(), image: #imageLiteral(resourceName: "library"), title: "Library")
+        ]
+    }
+    
+    private func generateViewController(rootViewController: UIViewController, image: UIImage, title: String) -> UIViewController {
+        let navigationVC = UINavigationController(rootViewController: rootViewController)
+        navigationVC.tabBarItem.image = image
+        navigationVC.tabBarItem.title = title
+        rootViewController.navigationItem.title = title
+        navigationVC.navigationBar.prefersLargeTitles = true
+        
+        return navigationVC
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
