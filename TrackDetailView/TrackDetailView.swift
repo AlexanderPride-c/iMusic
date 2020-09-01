df929a4a16475c4375ee7d97fce1e4268ffa0654
a.pride <a.pride.msk@gmail.com>
2020-09-01 14:07:00 +0300

    Track Detail Design Continued

--- TrackDetailView/TrackDetailView.swift
+++ TrackDetailView/TrackDetailView.swift
@@ -10,7 +10,40 @@ import UIKit
 
 class TrackDetailView: UIView {
     
+    
+    @IBOutlet weak var trackImageView: UIImageView!
+    @IBOutlet weak var currentTimeSlider: UISlider!
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var trackTitleLabel: UILabel!
+    @IBOutlet weak var authorTitleLabel: UILabel!
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var volumeSlider: UISlider!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        trackImageView.backgroundColor = .systemPink
+    }
+    
+    @IBAction func handleCurrentTimeSlider(_ sender: Any) {
+    }
+    
+    @IBAction func handleVolumeSlider(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func dragDownButton(_ sender: Any) {
+        self.removeFromSuperview()
+    }
+    
+    @IBAction func previouseTrack(_ sender: Any) {
+    }
+    
+    @IBAction func nextTrack(_ sender: Any) {
+    }
+    
+    @IBAction func playPauseAction(_ sender: Any) {
     }
 }
