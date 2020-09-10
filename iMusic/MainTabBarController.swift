d07bf0632360f20cf346b129195f6bcf159e1b0f
a.pride <a.pride.msk@gmail.com>
2020-09-10 19:03:30 +0300

    Dismissal UIPanGesture

--- iMusic/MainTabBarController.swift
+++ iMusic/MainTabBarController.swift
@@ -79,8 +79,8 @@ extension MainTabBarController: MainTabBarControllerDelegate {
     
     func maximizedTrackDetailController(viewModel: SearchViewModel.Cell?) {
         
-        maximizedTopAnchorConstraint.isActive = true
         minimizedTopAnchorConstraint.isActive = false
+        maximizedTopAnchorConstraint.isActive = true
         maximizedTopAnchorConstraint.constant = 0
         bottomAnchorConstraint.constant = 0
         
