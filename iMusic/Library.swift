0582d4f0e0f835e55c16a83478510139d105d085
a.pride <a.pride.msk@gmail.com>
2020-09-14 10:48:25 +0300

    button setting

--- iMusic/Library.swift
+++ iMusic/Library.swift
@@ -10,8 +10,36 @@ import SwiftUI
 
 struct Library: View {
     var body: some View {
+        NavigationView {
+            GeometryReader { geometry in
+                HStack {
+                    Button(action: {
+                        print("1234455435354")
+                    }, label: {
+                        Image(systemName: "play.fill")
+                            .frame(width: geometry.size.width / 2 - 10, height: 50)
+                            .accentColor(Color.init(#colorLiteral(red: 0.9098039216, green: 0.2666666667, blue: 0.3529411765, alpha: 1)))
+                            .background(Color.init(#colorLiteral(red: 0.8374180198, green: 0.8374378085, blue: 0.8374271393, alpha: 1)))
+                            .cornerRadius(10)
+                    })
+                    
+                    Button(action: {
+                        print("1234455435354")
+                    }, label: {
+                        Image(systemName: "arrow.2.circlepath")
+                            .frame(width: geometry.size.width / 2 - 10, height: 50)
+                            .accentColor(Color.init(#colorLiteral(red: 0.9098039216, green: 0.2666666667, blue: 0.3529411765, alpha: 1)))
+                            .background(Color.init(#colorLiteral(red: 0.8374180198, green: 0.8374378085, blue: 0.8374271393, alpha: 1)))
+                            .cornerRadius(10)
+                    })
+                }
+                }.padding().frame(height: 50)
+            
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationBarTitle("Library")
+        }
+    
         
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
