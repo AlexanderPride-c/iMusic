4b800f992ce1083fcc216fe556efc7fa10e64c29
a.pride <a.pride.msk@gmail.com>
2020-09-14 11:07:09 +0300

    Library View SwiftUI Setup

--- iMusic/Library.swift
+++ iMusic/Library.swift
@@ -11,35 +11,52 @@ import SwiftUI
 struct Library: View {
     var body: some View {
         NavigationView {
-            GeometryReader { geometry in
-                HStack {
-                    Button(action: {
-                        print("1234455435354")
-                    }, label: {
-                        Image(systemName: "play.fill")
-                            .frame(width: geometry.size.width / 2 - 10, height: 50)
-                            .accentColor(Color.init(#colorLiteral(red: 0.9098039216, green: 0.2666666667, blue: 0.3529411765, alpha: 1)))
-                            .background(Color.init(#colorLiteral(red: 0.8374180198, green: 0.8374378085, blue: 0.8374271393, alpha: 1)))
-                            .cornerRadius(10)
-                    })
-                    
-                    Button(action: {
-                        print("1234455435354")
-                    }, label: {
-                        Image(systemName: "arrow.2.circlepath")
-                            .frame(width: geometry.size.width / 2 - 10, height: 50)
-                            .accentColor(Color.init(#colorLiteral(red: 0.9098039216, green: 0.2666666667, blue: 0.3529411765, alpha: 1)))
-                            .background(Color.init(#colorLiteral(red: 0.8374180198, green: 0.8374378085, blue: 0.8374271393, alpha: 1)))
-                            .cornerRadius(10)
-                    })
-                }
+            VStack {
+                GeometryReader { geometry in
+                    HStack(spacing: 20) {
+                        Button(action: {
+                            print("1234455435354")
+                        }, label: {
+                            Image(systemName: "play.fill")
+                                .frame(width: geometry.size.width / 2 - 10, height: 50)
+                                .accentColor(Color.init(#colorLiteral(red: 0.9098039216, green: 0.2666666667, blue: 0.3529411765, alpha: 1)))
+                                .background(Color.init(#colorLiteral(red: 0.9181709285, green: 0.9272617298, blue: 0.9272617298, alpha: 1)))
+                                .cornerRadius(10)
+                        })
+                        
+                        Button(action: {
+                            print("1234455435354")
+                        }, label: {
+                            Image(systemName: "arrow.2.circlepath")
+                                .frame(width: geometry.size.width / 2 - 10, height: 50)
+                                .accentColor(Color.init(#colorLiteral(red: 0.9098039216, green: 0.2666666667, blue: 0.3529411765, alpha: 1)))
+                                .background(Color.init(#colorLiteral(red: 0.9181709285, green: 0.9272617298, blue: 0.9272617298, alpha: 1)))
+                                .cornerRadius(10)
+                        })
+                    }
                 }.padding().frame(height: 50)
-            
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                .navigationBarTitle("Library")
+                Divider().padding(.leading).padding(.trailing)
+                
+                List {
+                    LibraryCell()
+                }
+            }
+                
+            .navigationBarTitle("Library")
         }
-    
-        
+    }
+}
+
+
+struct LibraryCell: View {
+    var body: some View {
+        HStack {
+            Image("Image").resizable().frame(width: 60, height: 60).cornerRadius(2)
+            VStack {
+                Text("Track Name")
+                Text("Artist Name")
+            }
+        }
     }
 }
 
